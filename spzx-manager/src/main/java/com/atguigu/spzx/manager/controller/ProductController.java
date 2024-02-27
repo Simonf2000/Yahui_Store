aa93965e7907cad9bb86c5fb03b7a5a2b88da7fd
simonf2000 <simonf2000@outlook.com>
2024-02-27 20:00:05 +0800

    商品管理-后端接口

---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.atguigu.spzx.manager.controller;
+
+import com.atguigu.spzx.manager.service.ProductService;
+import com.atguigu.spzx.model.dto.product.ProductDto;
+import com.atguigu.spzx.model.entity.product.Product;
+import com.atguigu.spzx.model.vo.common.Result;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/admin/product/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService ;
+
+    @GetMapping(value = "/{page}/{limit}")
+    public Result<PageInfo<Product>> findByPage(@PathVariable Integer page, @PathVariable Integer limit, ProductDto productDto) {
+        PageInfo<Product> pageInfo = productService.findByPage(page, limit, productDto);
+        return Result.build(pageInfo , ResultCodeEnum.SUCCESS) ;
+    }
+
+}
