55558e006ff6ffc97bffa0ddc435f6eff908dbbe
simonf2000 <simonf2000@outlook.com>
2024-02-29 14:55:52 +0800

    一些菜单管理功能的的注释

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/helper/MenuHelper.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/helper/MenuHelper.java
@@ -14,10 +14,19 @@ import java.util.List;
  */
 public class MenuHelper {
 
+    /**
+    * @Description: 将菜单信息封装返回
+    * @Param: [sysMenuList]
+    * @return: java.util.List<com.atguigu.spzx.model.entity.system.SysMenu>
+    * @Author: simonf
+    */
     public static List<SysMenu> buildTree(List<SysMenu> sysMenuList) {
         List<SysMenu> sysMenuParentList = new ArrayList<>();
         for (SysMenu sysMenu : sysMenuList
         ) {
+            /**
+            * @Description: 寻找递归入口(ParentId为1的菜单)
+            */
             if (sysMenu.getParentId().intValue() == 0) {
                 sysMenuParentList.add(findChildren(sysMenu, sysMenuList));
             }
@@ -25,6 +34,12 @@ public class MenuHelper {
         return sysMenuParentList;
     }
 
+    /**
+    * @Description: 如果一个菜单的id是另一个菜单的ParentId，则另一个菜单就是他的孩子
+    * @Param: [sysMenuParent, sysMenuList]
+    * @return: com.atguigu.spzx.model.entity.system.SysMenu
+    * @Author: simonf
+    */
     private static SysMenu findChildren(SysMenu sysMenuParent, List<SysMenu> sysMenuList) {
         sysMenuParent.setChildren(new ArrayList<>());
         for (SysMenu menu : sysMenuList
