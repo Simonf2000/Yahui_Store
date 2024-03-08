bc5784cfc0fe532b269ba96a3b8dd4767496f2ca
simonf2000 <simonf2000@outlook.com>
2024-03-08 18:01:42 +0800

    前台系统-后端环境搭建

---
+++ spzx-service/service-product/src/main/java/com/atguigu/spzx/product/ProductApplication.java
@@ -0,0 +1,20 @@
+package com.atguigu.spzx.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: smionf
+ * @Date: 2024/03/08/17:40
+ * @Description:
+ */
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
