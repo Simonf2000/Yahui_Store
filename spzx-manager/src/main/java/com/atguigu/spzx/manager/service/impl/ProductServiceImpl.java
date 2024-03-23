37f678bb2affcc73eb4f7cb6d11a9ad15eb51fb2
simonf2000 <simonf2000@outlook.com>
2024-03-23 10:16:28 +0800

    前台系统-后端-商品详情-Redisson使用-重置布隆过滤器

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/ProductServiceImpl.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/ProductServiceImpl.java
@@ -11,16 +11,21 @@ import com.atguigu.spzx.model.entity.product.ProductDetails;
 import com.atguigu.spzx.model.entity.product.ProductSku;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBloomFilter;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 @Transactional
+@Slf4j
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
@@ -34,6 +39,9 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     RedissonClient redissonClient;
+
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @Override
     public PageInfo<Product> findByPage(Integer page, Integer limit, ProductDto productDto) {
@@ -102,6 +110,45 @@ public class ProductServiceImpl implements ProductService {
         productMapper.deleteById(id);
         productSkuMapper.deleteByProductId(id);
         productDetailsMapper.deletedByProductId(id);
+
+
+        //********重置布隆过滤器 start***********************************
+        //1.创建一个新的布隆过滤器
+        RBloomFilter<Object> bloomFilter = redissonClient.getBloomFilter(RedisConst.PRODUCT_NEW_BLOOM_FILTER);
+        bloomFilter.tryInit(1000000,0.00001); //插入数据量，误判率
+
+        //2.查询所有上架并且没有被软删除的skuId集合，放置到新的布隆过滤器中
+        List<Long> skuIdList =  productSkuMapper.findSkuIdAll();
+        for (Long skuId : skuIdList) {
+            bloomFilter.add(skuId);
+        }
+
+        //3.删除旧的布隆过滤器，将新的布隆过滤器名称修改为旧的布隆过滤器的名称。就更新布隆过滤器数据。保证原子性：lua脚本
+        System.out.println("skuIdList.toString() = " + skuIdList.toString());
+        // 定义lua脚本
+        String script = "if redis.call(\"exists\", KEYS[1]) == 1 then\n"+
+                "    redis.call(\"del\" , KEYS[1])\n"+
+                "end\n"+
+                "if redis.call(\"exists\", \"{\"..KEYS[1]..\"}:config\") == 1 then\n"+
+                "    redis.call(\"del\" , \"{\"..KEYS[1]..\"}:config\")\n"+
+                "end\n"+
+                "if redis.call(\"exists\", KEYS[2]) == 1 then\n"+
+                "    redis.call(\"rename\" , KEYS[2] , KEYS[1])\n"+
+                "end\n"+
+                "if redis.call(\"exists\", \"{\"..KEYS[2]..\"}:config\") == 1 then\n"+
+                "    redis.call(\"rename\" , \"{\"..KEYS[2]..\"}:config\" , \"{\"..KEYS[1]..\"}:config\")\n"+
+                "end\n"+
+                "return 1";
+        System.out.println("script = " + script);
+        // 执行lua脚本
+        //Long.class 表示脚本执行结果：1表示成功
+        redisTemplate.execute(new DefaultRedisScript<>(script , Long.class) ,
+                Arrays.asList(RedisConst.PRODUCT_BLOOM_FILTER , RedisConst.PRODUCT_NEW_BLOOM_FILTER)) ;
+
+        // 打印日志
+        log.info("reset bloomFilter成功了............");
+
+        //********重置布隆过滤器 end***********************************
     }
 
     @Override
