12ce37782dc3168c4d1c855dadd4c2a3e583dfd9
simonf2000 <simonf2000@outlook.com>
2024-03-25 10:42:23 +0800

    注册_发送验证码_搭建service-user服务

---
+++ spzx-service/service-user/src/main/java/com/atguigu/spzx/user/UserApplication.java
@@ -0,0 +1,20 @@
+package com.atguigu.spzx.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: smionf
+ * @Date: 2024/03/25/10:40
+ * @Description:
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
